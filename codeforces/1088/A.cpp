70fbdcef93d780af4acfb32497db9163193ae12d
Farhana Rahman <farhanajpj@gmail.com>
2021-07-18 21:19:00 +0600

    Add solution for problem `A - Ehab and another construction problem` Link: https://codeforces.com/contest/1088/problem/A Tags: brute force, constructive algorithms, *800 Ref: https://codeforces.com/contest/1088/submission/122944648

---
+++ codeforces/1088/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int n; cin>>n;
+
+    if(n==1)
+    {
+        cout<<"-1"<<endl;
+        return 0;
+    }
+
+
+    cout<<n<<" "<<n<<endl;
+}
