469afc9e3d0827cb42eb70434bef6555b12fdcdb
Farhana Rahman <farhanajpj@gmail.com>
2021-07-28 22:27:00 +0600

    Add solution for problem `A - Distance and Axis` Link: https://codeforces.com/contest/1401/problem/A Tags: constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1401/submission/124050712

---
+++ codeforces/1401/A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t; cin>>t;
+    while(t--)
+    {
+         int n,k; cin>>n>>k;
+         if(k<n)
+        {
+            if( (n-k)%2==0 )
+                cout<< "0" <<endl;
+            else
+                cout<< "1" <<endl;
+        }
+        else
+            cout<<k-n<<endl;
+    }
+}
