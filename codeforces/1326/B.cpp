96361086f199beaeeed40defda60c835431acef4
Farhana Rahman <farhanajpj@gmail.com>
2021-07-29 17:52:00 +0600

    Add solution for problem `B - Maximums` Link: https://codeforces.com/contest/1326/problem/B Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1326/submission/124115423

---
+++ codeforces/1326/B.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+//    int t; cin>>t;
+//    while(t--)
+//    {
+        int n; cin>>n;
+        ll a[n+5];
+        for(int i=0; i<n; i++)
+            cin>>a[i];
+
+        ll sum=0;
+        ll mx=0;
+        for(int i=0; i<n; i++)
+        {
+            sum=mx+a[i];
+            mx=max(mx,sum);
+            cout<<sum<<" ";
+        }
+        cout<<endl;
+
+
+//    }
+}
