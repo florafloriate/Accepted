59a3526f4201ca0b7392d722d7db9e3c4ffc3a1c
Farhana Rahman <farhanajpj@gmail.com>
2021-07-30 21:02:00 +0600

    Add solution for problem `A - PizzaForces` Link: https://codeforces.com/contest/1555/problem/A Tags: brute force, math, *900 Ref: https://codeforces.com/contest/1555/submission/124295790

---
+++ codeforces/1555/A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t; cin>>t;
+    while(t--)
+    {
+        ll n; cin>>n;
+
+        if(n<=6)
+            cout<<"15"<<endl;
+        else
+        {
+            if(n%2==0)
+            {
+                n/=2;
+                cout<< n*5 <<endl;
+            }
+            else
+            {
+                n++;
+                n/=2;
+                cout<< n*5 <<endl;
+            }
+        }
+    }
+}
