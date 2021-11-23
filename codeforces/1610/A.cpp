8efcf81355087929a643355f4464f3b3bc96b5b7
Farhana Rahman <farhanajpj@gmail.com>
2021-11-23 21:06:00 +0600

    Add solution for problem `A - Anti Light's Cell Guessing` Link: https://codeforces.com/contest/1610/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1610/submission/136639674

---
+++ codeforces/1610/A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int n,m; cin>>n>>m;
+
+        int mn=min(n,m);
+
+        if(n==1 && m==1)
+            cout<<"0"<<endl;
+        else if(mn==1)
+            cout<<"1"<<endl;
+        else
+            cout<<"2"<<endl;
+    }
+
+}
