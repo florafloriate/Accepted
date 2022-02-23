7f7328bbc57cc5140955a1367412a4e2be063afa
Farhana Rahman <farhanajpj@gmail.com>
2022-02-23 16:56:00 +0600

    Add solution for problem `B - Power Walking` Link: https://codeforces.com/contest/1642/problem/B Tags: greedy Ref: https://codeforces.com/contest/1642/submission/147439391

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-1 | [C - Great Sequence](https://codeforces.com/contest/1642/problem/C) | [GNU C++17](./codeforces/1642/C.cpp) |  | Feb/23/2022 17:43 | 
+2 | [C - Great Sequence](https://codeforces.com/contest/1642/problem/C) | [GNU C++17](./codeforces/1642/C.cpp) |  | Feb/23/2022 17:43 | 
+1 | [B - Power Walking](https://codeforces.com/contest/1642/problem/B) | [GNU C++17](./codeforces/1642/B.cpp) | `greedy` | Feb/23/2022 16:56 | 
---
+++ codeforces/1642/B.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t; cin>>t;
+    while(t--)
+    {
+       int n; cin>>n;
+       int a[n+5];
+       set<ll> s;
+       for(int i=0; i<n; i++)
+       {
+           cin>>a[i];
+           s.insert(a[i]);
+       }
+
+       int sz=s.size();
+
+       for(int i=1; i<=n; i++)
+       {
+           if(sz>=i)
+            cout<<sz<<" ";
+           else
+            cout<<i<<" ";
+       }
+       cout<<endl;
+    }
+}
