0508451a0e44c2e17f7e4bde3a4fe7d03cc06f94
Farhana Rahman <farhanajpj@gmail.com>
2022-04-03 11:04:00 +0600

    Add solution for problem `A - Vasya and Coins` Link: https://codeforces.com/contest/1660/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1660/submission/152541237

---
+++ codeforces/1660/A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t; cin>>t;
+    for(int tc=1; tc<=t; tc++)
+    {
+        int a,b; cin>>a>>b;
+
+        if(a==0)
+            cout<<"1"<<endl;
+        else
+            cout<<a*1+b*2+1<<endl;
+    }
+
+    return 0;
+}
