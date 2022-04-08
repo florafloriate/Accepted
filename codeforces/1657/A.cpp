edd3de4ab1ab6e19570b7ddf0546ccb570847dc5
Farhana Rahman <farhanajpj@gmail.com>
2022-04-08 11:42:00 +0600

    Add solution for problem `A - Integer Moves` Link: https://codeforces.com/contest/1657/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1657/submission/152982305

---
+++ codeforces/1657/A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int x,y; cin>>x>>y;
+
+        double d=sqrt( (x*x) + (y*y) );
+
+        int a=ceil(d),b=floor(d);
+
+        if(x==0 && y==0)
+            cout<<"0"<<endl;
+        else if(a==b)
+            cout<<"1"<<endl;
+        else
+            cout<<"2"<<endl;
+    }
+
+}
