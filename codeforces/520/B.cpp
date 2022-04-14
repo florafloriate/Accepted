a36adf59c3c54c41b6cbe7965474f99120caf598
Farhana Rahman <farhanajpj@gmail.com>
2022-04-15 00:33:00 +0600

    Add solution for problem `B - Two Buttons` Link: https://codeforces.com/contest/520/problem/B Tags: dfs and similar, graphs, greedy, implementation, math, shortest paths, *1400 Ref: https://codeforces.com/contest/520/submission/153667967

---
+++ codeforces/520/B.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int n,m,ans=0; cin>>n>>m;
+
+    while(n<m)
+    {
+        if(m%2==0)
+            m/=2;
+        else
+            m++;
+
+        ans++;
+    }
+
+    cout<<ans+n-m<<endl;
+}
