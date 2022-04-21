2b3331912abc8ae290d4efde6d536f95b14ace5b
Farhana Rahman <farhanajpj@gmail.com>
2022-04-21 20:41:00 +0600

    Add solution for problem `B - Triple` Link: https://codeforces.com/contest/1669/problem/B Tags: implementation, sortings, *800 Ref: https://codeforces.com/contest/1669/submission/154303148

---
+++ codeforces/1669/B.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define bug(x)           cout<<x<<"*"<<endl;
+#define LL_INF           1LL<<62
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        int a[n+5];
+        map<int,int> m;
+        for(int i=0; i<n; i++)
+        {
+            cin>>a[i];
+            m[a[i]]++;
+        }
+        bool f=false;
+        for(auto it:m)
+        {
+            if(it.second>=3)
+            {
+                cout<<it.first<<endl;
+                f=true;
+                break;
+            }
+        }
+
+        if(f==false)
+            cout<<"-1"<<endl;
+    }
+}
