7aa1ccf3c5adc9c4195e3ee9565d86fa5eb3af3b
Farhana Rahman <farhanajpj@gmail.com>
2022-04-22 05:04:00 +0600

    Add solution for problem `F - Eating Candies` Link: https://codeforces.com/contest/1669/problem/F Tags: binary search, data structures, greedy, two pointers, *1100 Ref: https://codeforces.com/contest/1669/submission/154446181

---
+++ codeforces/1669/F.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define LL_INF           1LL<<62
+#define Checkbit(x, k)   (x & (1LL << k)) ///whether the k-th bit of x is zero or not
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define binary(x)        bitset<31>(x)
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        int a[n+5];
+        for(int i=0; i<n; i++)
+            cin>>a[i];
+
+        int lsum=0, rsum=0;
+        int cnt=0, ans=0;
+
+        for(int left=0,right=n-1; left<=right;)
+        {
+            if(lsum<=rsum)
+            {
+                lsum+=a[left];
+                left++; cnt++;
+            }
+            else if(lsum>rsum)
+            {
+                rsum+=a[right];
+                right--; cnt++;
+            }
+
+            if(lsum==rsum)
+            {
+                ans=cnt;
+            }
+
+        }
+
+        cout<<ans<<endl;
+    }
+}
